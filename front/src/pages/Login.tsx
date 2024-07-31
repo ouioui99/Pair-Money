7a755612405bf8083bcd8ebe6a6cb6cf3f0c3f01
ouioui99 <qp5minute@gmail.com>
2024-07-31 23:18:38 +0900

    ログイン画面作成

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import TextFormInput from "../components/TextFormInput";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-md">
+        <h2 className="text-2xl font-bold text-center">ログイン</h2>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              メールアドレス
+            </label>
+            <TextFormInput
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required={true}
+              value={email}
+              onChange={setEmail}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              パスワード
+            </label>
+            <TextFormInput
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required={true}
+              value={password}
+              onChange={setPassword}
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              ログイン
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
