483f3cf5dbe9cf9d2f5d7b7d54ddff324fd82326
ouioui99 <qp5minute@gmail.com>
2024-08-17 12:27:41 +0900

    localhostをhttps化

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
+    https: true,
     // External Publication
     host: "0.0.0.0",
     // You can change the port for starting up.
