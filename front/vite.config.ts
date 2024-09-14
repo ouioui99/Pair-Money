7f568f32ab0709da450c4871ca41dc313734c725
ouioui99 <qp5minute@gmail.com>
2024-09-14 16:17:43 +0900

    環境変数ファイルから値を持ってくる設定

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
+import env from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), mkcert()],
+  plugins: [
+    react(),
+    mkcert(),
+    env({ prefix: "VITE", mountedPath: "process.env" }),
+  ],
   server: {
     https: true,
     // External Publication
