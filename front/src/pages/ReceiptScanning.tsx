9ec18a72bf242d95fa1fd87c750b4047f2f8afe6
ouioui99 <qp5minute@gmail.com>
2025-05-02 08:49:41 +0900

    カメラボタンのアイコン位置修正

--- front/src/pages/ReceiptScanning.tsx
+++ front/src/pages/ReceiptScanning.tsx
@@ -31,13 +31,13 @@ export default function ReceiptScanning() {
       />
       <button
         type="button"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text- p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full p-2.5 text-center inline-flex items-center justify-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         style={{
           width: "70px",
           height: "70px",
           position: "absolute",
           top: "550px",
-          zIndex: 10, // ボタンを前面に表示
+          zIndex: 10,
         }}
         onClick={() => {
           if (camera.current) {
